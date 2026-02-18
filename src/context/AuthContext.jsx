3f87f06fed98e4996b87b57b5537346e5de15800
gourav <kartikpal1402@gmail.com>
2026-02-18 10:17:08 +0530

    add: auth context

---
+++ src/context/AuthContext.jsx
@@ -0,0 +1,58 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import { auth } from "../firebase";
+import {
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword,
+    signOut,
+    updateProfile,
+    onAuthStateChanged
+} from "firebase/auth";
+
+const AuthContext = createContext();
+
+export function AuthProvider({ children }) {
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    // Register
+    const register = async (name, email, password) => {
+        const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+
+        await updateProfile(userCredential.user, {
+            displayName: name
+        });
+
+        return userCredential.user;
+    };
+
+    // Login
+    const login = (email, password) => {
+        return signInWithEmailAndPassword(auth, email, password);
+    };
+
+    // Logout
+    const logout = () => {
+        return signOut(auth);
+    };
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+            setUser(currentUser);
+            setLoading(false);
+        });
+
+        return unsubscribe;
+    }, []);
+
+    const value = {
+        user,
+        register,
+        login,
+        logout
+    };
+
+    return <AuthContext.Provider value={value}>{!loading && children}</AuthContext.Provider>;
+}
+
+// Custom hook
+export const useAuth = () => useContext(AuthContext);
