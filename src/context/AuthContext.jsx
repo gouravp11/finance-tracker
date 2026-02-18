b24f0b0f6fc78e4bc4101c8fb54adb00fd00bc35
gourav <kartikpal1402@gmail.com>
2026-02-18 11:57:49 +0530

    fix: imports in auth context

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,5 +1,5 @@
 import { createContext, useContext, useEffect, useState } from "react";
-import { auth } from "../firebase";
+import { auth } from "../config/firebase";
 import {
     createUserWithEmailAndPassword,
     signInWithEmailAndPassword,
